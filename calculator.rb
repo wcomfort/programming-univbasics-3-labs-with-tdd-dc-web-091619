8388e82c06c58060aede2dd6b5125ad39b6af344
Will Comfort <comfortwb21@gmail.com>
2019-08-23 02:59:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ first_number = 21
 second_number = 4
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
